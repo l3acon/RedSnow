3a7fa99ff2167429f0c17787a230f10cbb05c98e
l3acon@gmail.com <l3acon@gmail.com>
2013-09-11 21:05:25 -0600

    my subliminal changes

---
+++ Assets/RocketLauncherController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RocketLauncherController : WeaponController {
+
+	public Rigidbody RocketObject;
+	public Vector3 RocketOffset;
+	//public Vector3 RocketRotation;
+
+	private List<Transform> RocketTransforms;
+
+
+	void Attack()
+	{
+		Instantiate(RocketObject, RocketObject.position + RocketOffset, Quaternion.identity);
+		RocketObject.AddForce(OurPlayer.transform.rotation.eulerAngles);
+
+	}
+}
