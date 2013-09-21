6306e4dca0258b9e0736cf815edfb3325da71730
Esteban Martinez <ecmartz.nm@gmail.com>
2013-09-21 15:12:09 -0600

    Minimap: camera prefab, script, and demo in ecmartzScene

---
+++ Assets/minimapFollow.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+/// <summary>
+/// Minimap follow.cs
+/// Script that makes the minimap follow the player. Very simple.
+/// author: ecmartz
+/// </summary>
+public class minimapFollow : MonoBehaviour 
+{
+	public Transform target;
+	public float height = 5.0f;
+	
+	void Start () {
+		//Go to player!
+		transform.position = new Vector3(target.position.x,target.transform.position.y+height,target.position.z);
+	}
+	
+	
+	void LateUpdate()
+	{
+		//Move with player
+		transform.position = new Vector3(target.position.x,transform.position.y,target.position.z);
+	}
+}
