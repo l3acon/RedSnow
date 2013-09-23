cf8d422bd0e451f4200fbeb0990c0388ed191911
Kurt Hollowell <kurtdog@unm.edu>
2013-09-22 23:03:33 -0600

    Media Folder, and Gui stuff for the mini map - minimapFollow.cs

--- Assets/minimapFollow.cs
+++ Assets/minimapFollow.cs
@@ -1,7 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-
 /// <summary>
 /// Minimap follow.cs
 /// Script that makes the minimap follow the player. Very simple.
@@ -11,6 +10,7 @@ public class minimapFollow : MonoBehaviour
 {
 	public Transform target;
 	public float height = 5.0f;
+	public Texture2D playerPic;
 	
 	void Start () {
 		//Go to player!
@@ -22,5 +22,13 @@ public class minimapFollow : MonoBehaviour
 	{
 		//Move with player
 		transform.position = new Vector3(target.position.x,transform.position.y,target.position.z);
+		
 	}
+	
+	// gui stuff was found here: http://docs.unity3d.com/Documentation/ScriptReference/GUI.Label.html
+	void OnGUI() {
+		GUI.color = Color.red; // set the color
+        GUI.Label(new Rect(100, 20, 100, 20), "P1"); // draw text
+		//GUI.Label(new Rect(10, 20, 29, 37),playerPic ); // draw a texture
+    }
 }
