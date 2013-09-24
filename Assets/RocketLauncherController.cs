924a7cf1cb6ceafdd03fb6c9a7ab2213bf2e284c
l3acon@gmail.com <l3acon@gmail.com>
2013-09-23 22:06:19 -0600

    updates to controller

--- Assets/RocketLauncherController.cs
+++ Assets/RocketLauncherController.cs
@@ -2,21 +2,14 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class RocketLauncherController : MonoBehaviour {
+public class RocketLauncherController : WeaponController {
 
 	public Rigidbody RocketObject;
 	public Vector3 RocketOffset;
 	public Transform RocketTransform;
 	//public Vector3 RocketRotation;
 
-	public GameObject WeaponObject;
-	public Rigidbody OurPlayer;
-	public Vector3 weaponOffset;
-	public float attackRate;
-
 	public float rocketForce = 2f;
-	protected float nextAttack = 3f;
-	protected bool iAttack;
 	public List<Rigidbody> Rockets;
 
 	void Start()	//instanciate our weapon and make it our child
@@ -26,29 +19,24 @@ public class RocketLauncherController : MonoBehaviour {
 		clone.transform.parent = OurPlayer.transform;
 
 		nextAttack = attackRate;
+		Setup();
 	}
 
-	void Update()
-	{
-		iAttack = Input.GetButton("Fire1");
-	}
 
-	void Attack()
+	override protected void Attack()
 	{
 		Rockets.Add(Instantiate(RocketObject, OurPlayer.position + RocketOffset, Quaternion.identity) as Rigidbody);
+		Debug.Log("shooting");
 	}
 
 	void FixedUpdate()
 	{
-		if(iAttack && (Time.time > nextAttack) )
-		{
-			nextAttack = Time.time + attackRate;
-			Attack();
-		}
+		AttackCheck();
 
 		foreach (Rigidbody rocket in Rockets)
 		{
 			rocket.AddForce(rocketForce * rocket.rotation.eulerAngles);
 		}
+
 	}
 }
